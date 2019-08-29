47f73bf795156159cd07de6eca696966068809c6
Lili <Lilianasadaniowski@gmail.com>
2019-08-28 22:14:00 -0300

    tipo de dato limitado

--- usuario_ingresa.py
+++ usuario_ingresa.py
@@ -1,4 +1,11 @@
-a = int (input('ingrese un numero: '))
-b = int (input('ingrese otro numero: '))
-sum = a+b
-print(sum)
+
+while True:
+    try:
+        a = int(input('ingrese un nro : ' ))
+        b = int(input(' ingrese otro nro: '))
+        sum = a+b
+        print('la suma es :', sum,)
+        break
+    except:
+        print ('El dato ingresado no es entero')
+        pass
