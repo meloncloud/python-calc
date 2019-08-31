6ea85474da375782983e69c5689626ac1358e530
Lili <Lilianasadaniowski@gmail.com>
2019-08-31 09:40:48 -0300

    cree prop DNI

--- class_Persona.py
+++ class_Persona.py
@@ -1,13 +1,14 @@
 class Persona:
 
-    def cargar_datos (self,nom, edad, altura):
+    def cargar_datos (self,nom, edad, altura, DNI):
         self.nombre = nom
         self.edad = edad
         self.altura = altura
+        self.DNI = DNI
 
     def imprimir(self):
-        print('Su Nombre : ',self.nombre, 'y su edad es:', self.edad, 'anos', 'Su altura es:' , self.altura)
+        print('Su Nombre : ',self.nombre, 'y su edad es:', self.edad, 'anos', 'Su altura es:' , self.altura, 'DNI : ', self.DNI)
 
 persona1=Persona()
-persona1.cargar_datos("Pedro", "32", "1,70 mts")
+persona1.cargar_datos("Pedro", "32", "1,70 mts", "27981816")
 persona1.imprimir()
