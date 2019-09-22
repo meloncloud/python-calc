bf1cd4eb2c4bef8e0c2afd964a320a59c3097a59
Lili <Lilianasadaniowski@gmail.com>
2019-09-22 11:54:29 -0300

    Sumar 2 variables

--- Clase_Sumadora.py
+++ Clase_Sumadora.py
@@ -1,2 +1,12 @@
 class Sumadora:
-   
+
+   def __init__(self, num1 , num2):
+   	    self.num1 = num1
+   	    self.num2 = num2
+
+   def sum (self):
+   	      Suma = self.num1 + self.num2
+   	      return Suma
+
+a = Sumadora (2 , 4)
+print (a)
