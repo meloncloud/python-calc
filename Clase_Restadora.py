192b5891808727e691ccb64b0c22a33cd1a17ef8
Lili <Lilianasadaniowski@gmail.com>
2019-09-22 22:21:41 -0300

    Complete el ejercicio

---
+++ Clase_Restadora.py
@@ -0,0 +1,15 @@
+class Restadora:
+
+   def __init__(self, num1 , num2):
+   	    self.num1 = num1
+   	    self.num2 = num2
+
+   def rest (self):
+   	    if self > 0:
+   	      print( self.num1 - self.num2)
+   #def Absum(self):
+   	#      print("")
+
+
+resta = Restadora(8,-4)
+resta .rest()
