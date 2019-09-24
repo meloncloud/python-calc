c57a686edd7343e5ef9cc0edc2affcaf8e3d1e5f
Lili <Lilianasadaniowski@gmail.com>
2019-09-23 23:30:32 -0300

    modifique el texto

--- Clase_Restadora.py
+++ Clase_Restadora.py
@@ -7,7 +7,7 @@ class Restadora:
        if self.num2 != 0:     
    	    print( self.num1 - self.num2)
        else:
-          print( ' ingrese un nro mayor que cero')
+          print( ' ingrese un numero mayor que cero')
           
 resta = Restadora(8,9)
 resta .rest()
